66bad0dd136b20c559827395d53e64c2ce214459
gy31052000 <33536030+gy31052000@users.noreply.github.com>
2017-11-15 16:27:46 +0800

    123

--- githubtest/Form1.cs
+++ githubtest/Form1.cs
@@ -16,5 +16,10 @@ namespace githubtest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //hello
+        }
     }
 }
